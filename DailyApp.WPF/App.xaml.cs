e7d8ab2b8c45421f74228e9d3b3d8d847a9fc366
Evan <evanz1215@gmail.com>
2024-06-09 13:03:17 +0800

    加入專案檔案。

---
+++ DailyApp.WPF/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using DailyApp.WPF.ViewModels;
+using DailyApp.WPF.Views;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Services.Dialogs;
+using System.Windows;
+
+namespace DailyApp.WPF;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : PrismApplication
+{
+    protected override Window CreateShell()
+    {
+        return Container.Resolve<MainWindow>();
+    }
+
+    /// <summary>
+    /// 依賴注入
+    /// </summary>
+    /// <param name="containerRegistry"></param>
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        containerRegistry.RegisterDialog<LoginUC, LoginUCViewModel>();
+    }
+
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        // 判斷是否已經登入
+        var dialog = Container.Resolve<IDialogService>();
+        dialog.ShowDialog("LoginUC", callback =>
+        {
+            if (callback.Result != ButtonResult.OK)
+            {
+                Environment.Exit(0);
+                return;
+            }
+            base.OnInitialized();
+        });
+    }
+}
---
+++ DailyApp.WPF/ViewModels/LoginUCViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Commands;
+using Prism.Services.Dialogs;
+
+namespace DailyApp.WPF.ViewModels;
+
+public class LoginUCViewModel : IDialogAware
+{
+    public string Title { get; set; } = "我的日常";
+
+    public DelegateCommand LoginComm { get; set; }
+
+
+    public event Action<IDialogResult> RequestClose;
+
+    public LoginUCViewModel()
+    {
+        LoginComm = new DelegateCommand(LoginHandler);
+    }
+
+
+    /// <summary>
+    /// 登入方法
+    /// </summary>
+    private void LoginHandler()
+    {
+        // 模擬登入成功
+        RequestClose?.Invoke(new DialogResult(ButtonResult.OK));
+    }
+
+
+    public bool CanCloseDialog()
+    {
+        return true;
+    }
+
+    public void OnDialogClosed()
+    {
+    }
+
+    public void OnDialogOpened(IDialogParameters parameters)
+    {
+    }
+}
