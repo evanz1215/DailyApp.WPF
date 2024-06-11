05697ac421ddd9498d2fed10348777165c9b9c68
Evan <evanz1215@gmail.com>
2024-06-11 22:33:46 +0800

    左側菜單樣式

---
+++ DailyApp.WPF/Models/LeftMenuInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace DailyApp.WPF.Models;
+
+/// <summary>
+/// 主介面左側菜單
+/// </summary>
+public class LeftMenuInfo
+{
+    /// <summary>
+    /// Icon
+    /// </summary>
+    public string Icon { get; set; }
+
+    /// <summary>
+    /// 菜單名稱
+    /// </summary>
+    public string MenuName { get; set; }
+
+    /// <summary>
+    /// 視圖名稱
+    /// </summary>
+    public string ViewName { get; set; }
+}
--- DailyApp.WPF/ViewModels/MainWindowViewModel.cs
+++ DailyApp.WPF/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DailyApp.WPF.Models;
+using Prism.Mvvm;
 
 namespace DailyApp.WPF.ViewModels;
-public class MainWindowViewModel
-{
 
+public class MainWindowViewModel : BindableBase
+{
     public MainWindowViewModel()
     {
+        LeftMenuList = new List<LeftMenuInfo>();
+        this.CreateMenu();
+    }
 
+    #region 左側菜單
+
+    private List<LeftMenuInfo> _leftMenuList;
+
+    public List<LeftMenuInfo> LeftMenuList
+    {
+        get { return _leftMenuList; }
+        set
+        {
+            _leftMenuList = value;
+            RaisePropertyChanged();
+        }
+    }
+
+    #endregion 左側菜單
+
+    /// <summary>
+    /// 創建菜單數據
+    /// </summary>
+    private void CreateMenu()
+    {
+        LeftMenuList.Add(new LeftMenuInfo() { Icon = "Home", MenuName = "首頁", ViewName = "IndexView" });
+        LeftMenuList.Add(new LeftMenuInfo() { Icon = "NotebookOutline", MenuName = "代辦事項", ViewName = "Waitiew" });
+        LeftMenuList.Add(new LeftMenuInfo() { Icon = "NotebookPlus", MenuName = "備忘錄", ViewName = "MemoView" });
+        LeftMenuList.Add(new LeftMenuInfo() { Icon = "Cog", MenuName = "設置", ViewName = "SettingView" });
     }
 }
