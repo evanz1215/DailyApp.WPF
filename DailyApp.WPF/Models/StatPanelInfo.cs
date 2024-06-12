c4e36c8f5406f09e19c00ae4e20c73b544caab67
Evan <evanz1215@gmail.com>
2024-06-12 22:28:38 +0800

    首頁統計面板

---
+++ DailyApp.WPF/Models/StatPanelInfo.cs
@@ -0,0 +1,33 @@
+﻿namespace DailyApp.WPF.Models
+{
+    /// <summary>
+    /// 首頁統計面板
+    /// </summary>
+    public class StatPanelInfo
+    {
+        /// <summary>
+        /// 圖示
+        /// </summary>
+        public string Icon { get; set; }
+
+        /// <summary>
+        /// 名稱
+        /// </summary>
+        public string ItemName { get; set; }
+
+        /// <summary>
+        /// 統計結果
+        /// </summary>
+        public string Result { get; set; }
+
+        /// <summary>
+        /// 背景顏色
+        /// </summary>
+        public string BackColor { get; set; }
+
+        /// <summary>
+        /// 跳轉
+        /// </summary>
+        public string ViewName { get; set; }
+    }
+}
--- DailyApp.WPF/ViewModels/HomeUCViewModel.cs
+++ DailyApp.WPF/ViewModels/HomeUCViewModel.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DailyApp.WPF.Models;
+using Prism.Mvvm;
 
 namespace DailyApp.WPF.ViewModels
 {
-    public class HomeUCViewModel
+    public class HomeUCViewModel : BindableBase
     {
+        public HomeUCViewModel()
+        {
+            this.CreateStatPanel();
+        }
+
+        private List<StatPanelInfo> _statPanelList;
+
+        public List<StatPanelInfo> StatPanelList
+        {
+            get { return _statPanelList; }
+            set
+            {
+                _statPanelList = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private void CreateStatPanel()
+        {
+            StatPanelList = new List<StatPanelInfo>();
+
+            StatPanelList.Add(new StatPanelInfo() { Icon = "ClockFast", ItemName = "彙總", BackColor = "#FF0CA0FF", ViewName = "WaitUC", Result = "9" });
+            StatPanelList.Add(new StatPanelInfo() { Icon = "ClockCheckOutline", ItemName = "已完成", BackColor = "#FF1ECA3A", ViewName = "WaitUC", Result = "9" });
+            StatPanelList.Add(new StatPanelInfo() { Icon = "ChartLineVariant", ItemName = "完成比例", BackColor = "#FF02C6DC", Result = "90%" });
+            StatPanelList.Add(new StatPanelInfo() { Icon = "PlaylistStar", ItemName = "備忘錄", BackColor = "#FFFFA000", ViewName = "MemoUC", Result = "20" });
+        }
     }
 }
